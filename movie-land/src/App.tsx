f6eca5df9a54f886d9fc7aa7119a35fa434306f3
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-11-18 13:00:59 +0100

    render one movie search on the dom

--- movie-land/src/App.tsx
+++ movie-land/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import './App.css';
-import SearchIcon from './search-icon.svg';
+import SearchIcon from './search.svg';
 
 // eccaa7da
 const API_URL = 'http://www.omdbapi.com?apikey=eccaa7da';
@@ -16,7 +16,7 @@ function App() {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log('movie data', data);
+    console.log('movie data', data.Search);
 
     setMovies(data.Search);
   };
@@ -27,8 +27,35 @@ function App() {
       <div className="search">
         <input
           type="text"
-          placeholder="Search for a movie"
+          placeholder="Search for movies"
+          onChange={(e) => {}}
+          value={'star wars'}
         />
+        <img
+          src={SearchIcon}
+          alt="search icon"
+          onClick={() => {}}
+        />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{MovieSearch.Year}</p>
+          </div>
+
+          <div>
+            <img
+              src={MovieSearch.Poster === 'N/A' ? 'https://via.placeholder.com/400' : MovieSearch.Poster}
+              alt={MovieSearch.Title}
+            />
+          </div>
+
+          <div>
+            <span>{MovieSearch.Type}</span>
+            <h3>{MovieSearch.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -40,4 +67,22 @@ type Movie = {
   Search: Array<Record<string, any>>;
   totalResults: string;
   Response: string;
+};
+
+type TMovieSearch = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
+
+const MovieSearch: TMovieSearch = {
+  Title: 'Star Wars: Episode IV - A New Hope',
+  Year: '1977',
+  imdbID: 'tt0076759',
+  Type: 'movie',
+  Poster: 'N/A',
+  // Poster:
+  // 'https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg',
 };
